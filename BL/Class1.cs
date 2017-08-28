2968d2024c7288d3884dc9af7d593972b2575af8
SELS <SELS@LIN35001181>
2017-08-28 19:12:52 +0530

    Signed-off-by: SELS <SELS@LIN35001181>

---
+++ BL/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Class1
+    {
+        public int Add(int a, int b)
+        {
+            int i;
+            return a + b;
+            int j;
+        }
+    }
+}
